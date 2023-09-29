2c893678cf3dd420ef310baed82d071d4a285b65
KKnnss <109391833+KKnnss@users.noreply.github.com>
2023-09-29 13:16:31 -0300

    Add files via upload

---
+++ perfil/form/form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pro-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+}
